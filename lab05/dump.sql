023a8cebb45c9d7cd216f458cd419a63e31d335e
m3tro1d <qazwf734@gmail.com>
2022-03-11 11:14:14 +0300

    lab5: add dump and some tasks

---
+++ lab05/dump.sql
@@ -0,0 +1,66 @@
+CREATE TABLE client
+(
+    id_client INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    name      VARCHAR(511) NOT NULL,
+    phone     VARCHAR(30)  NOT NULL
+);
+
+CREATE TABLE booking
+(
+    id_booking   INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    id_client    INT UNSIGNED NOT NULL,
+    booking_date DATE         NOT NULL,
+    CONSTRAINT booking_client_id_client_fk
+        FOREIGN KEY (id_client) REFERENCES client (id_client)
+            ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE hotel
+(
+    id_hotel INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    stars    TINYINT(1)   NOT NULL
+);
+
+CREATE TABLE room_category
+(
+    id_room_category INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    name             VARCHAR(255) NOT NULL,
+    square           INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE room
+(
+    id_room          INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    id_hotel         INT UNSIGNED   NOT NULL,
+    id_room_category INT UNSIGNED   NOT NULL,
+    number           INT UNSIGNED   NOT NULL,
+    price            DECIMAL(15, 2) NOT NULL,
+    CONSTRAINT room_hotel_id_hotel_fk
+        FOREIGN KEY (id_hotel) REFERENCES hotel (id_hotel)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT room_room_category_id_room_category_fk
+        FOREIGN KEY (id_room_category) REFERENCES room_category (id_room_category)
+            ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE room_in_booking
+(
+    id_room_in_booking INT UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    id_booking         INT UNSIGNED NOT NULL,
+    id_room            INT UNSIGNED NOT NULL,
+    checkin_date       DATE         NOT NULL,
+    checkout_date      DATE         NOT NULL,
+    CONSTRAINT room_in_booking_booking_id_booking_fk
+        FOREIGN KEY (id_booking) REFERENCES booking (id_booking)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT room_in_booking_room_id_room_fk
+        FOREIGN KEY (id_room) REFERENCES room (id_room)
+            ON UPDATE CASCADE ON DELETE CASCADE
+);
---
+++ lab05/lab5.sql
@@ -0,0 +1,84 @@
+USE lab05;
+
+
+-- 1. Добавить внешние ключи.
+ALTER TABLE booking
+    ADD CONSTRAINT booking_client_id_client_fk
+        FOREIGN KEY (id_client) REFERENCES client (id_client)
+            ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE room
+    ADD CONSTRAINT room_hotel_id_hotel_fk
+        FOREIGN KEY (id_hotel) REFERENCES hotel (id_hotel)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT room_room_category_id_room_category_fk
+        FOREIGN KEY (id_room_category) REFERENCES room_category (id_room_category)
+            ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE room_in_booking
+    ADD CONSTRAINT room_in_booking_booking_id_booking_fk
+        FOREIGN KEY (id_booking) REFERENCES booking (id_booking)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT room_in_booking_room_id_room_fk
+        FOREIGN KEY (id_room) REFERENCES room (id_room)
+            ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+-- 2. Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах категории “Люкс” на 1 апреля 2019г.
+SELECT c.name, c.phone
+FROM client c
+         INNER JOIN booking b ON c.id_client = b.id_client
+         INNER JOIN room_in_booking rib ON b.id_booking = rib.id_booking AND rib.checkin_date <= '2019-04-01' AND
+                                           rib.checkout_date >= '2019-04-01'
+         INNER JOIN room r ON rib.id_room = r.id_room
+         INNER JOIN hotel h ON r.id_hotel = h.id_hotel AND h.name = 'Космос'
+         INNER JOIN room_category rc ON r.id_room_category = rc.id_room_category AND rc.name = 'Люкс';
+
+
+-- 3. Дать список свободных номеров всех гостиниц на 22 апреля.
+-- TODO: duplicated rows
+SELECT h.name, r.number, r.price, rc.name
+FROM room r
+         INNER JOIN room_in_booking rib ON r.id_room = rib.id_room AND NOT (rib.checkin_date <= '2019-04-22' AND
+                                                                            rib.checkout_date >= '2019-04-22')
+         INNER JOIN hotel h ON r.id_hotel = h.id_hotel
+         INNER JOIN room_category rc ON r.id_room_category = rc.id_room_category;
+
+
+-- 4. Дать количество проживающих в гостинице “Космос” на 23 марта по каждой категории номеров
+SELECT rc.name, COUNT(c.id_client) AS residents_amount
+FROM client c
+         INNER JOIN booking b ON c.id_client = b.id_client
+         INNER JOIN room_in_booking rib ON b.id_booking = rib.id_booking AND rib.checkin_date <= '2019-03-23' AND
+                                           rib.checkout_date >= '2019-03-23'
+         INNER JOIN room r ON rib.id_room = r.id_room
+         INNER JOIN room_category rc ON r.id_room_category = rc.id_room_category
+         INNER JOIN hotel h ON r.id_hotel = h.id_hotel AND h.name = 'Космос'
+GROUP BY rc.name;
+
+
+-- 5. Дать список последних проживавших клиентов по всем комнатам гостиницы “Космос”,
+-- выехавшим в апреле с указанием даты выезда.
+-- TODO
+
+
+-- 6. Продлить на 2 дня дату проживания в гостинице “Космос” всем клиентам комнат
+-- категории “Бизнес”, которые заселились 10 мая.
+-- TODO
+
+
+-- 7. Найти все "пересекающиеся" варианты проживания. Правильное состояние: не
+-- может быть забронирован один номер на одну дату несколько раз, т.к. нельзя
+-- заселиться нескольким клиентам в один номер. Записи в таблице
+-- room_in_booking с id_room_in_booking = 5 и 2154 являются примером
+-- неправильного состояния, которые необходимо найти. Результирующий кортеж
+-- выборки должен содержать информацию о двух конфликтующих номерах.
+-- TODO
+
+
+-- 8. Создать бронирование в транзакции.
+-- TODO
+
+
+-- 9. Добавить необходимые индексы для всех таблиц.
+-- TODO
